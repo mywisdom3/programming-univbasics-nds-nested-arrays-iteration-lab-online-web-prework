916eb92b5a3cdc613ffa54646ddf13ed5b82d3ed
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-28 17:39:55 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -34,4 +34,16 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  total_even_pairs = [
+    [86262, 58115],
+    [22558, 97709],
+    [66236, 28056], 
+    [25992, 81375], 
+    [11248, 56789],
+    [42783, 27353],
+    [70796, 63432],
+    [53234, 63058], 
+    [72316, 8675309]
+    ]
+    
 end
