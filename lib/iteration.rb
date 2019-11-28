9f3a74a64e53a7a0cabe34d93996f2f3bedd3d78
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-28 17:46:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -19,7 +19,7 @@ def find_greater_pair(src)
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   find_greater_pair =[
-    [[-1, -900],
+    [-1, -900],
     [10, 30], 
     [0, 0], 
     [14, 16 * -2.5]
@@ -34,16 +34,5 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-  total_even_pairs = [
-    [86262, 58115],
-    [22558, 97709],
-    [66236, 28056], 
-    [25992, 81375], 
-    [11248, 56789],
-    [42783, 27353],
-    [70796, 63432],
-    [53234, 63058], 
-    [72316, 8675309]
-    ]
-   
+  
 end
